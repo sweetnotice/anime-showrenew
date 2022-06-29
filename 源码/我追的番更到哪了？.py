a8d1c826c6739a4bbd6b792c849102e014052248
sweetnotice <3306601284@qq.com>
2022-06-29 18:04:14 +0800

    更新v1.7
    优化了代码书写
    优化了输出显示，之前是打印追番目录后再重试，现在是重试完在打印，简化了输出看着不乱
    增加了超时重试次数限制，到了上线会提示稍后再运行

--- 源码/我追的番更到哪了？.py
+++ 源码/我追的番更到哪了？.py
@@ -30,9 +30,7 @@ def get_links():
 
 def finish_anime_name_link():
     if len(finish_names) != 0:
-        show_finish_list = str(finish_names).replace("['", '').replace("']", '').replace("'", '').replace(',',
-                                                                                                          '\n').replace(
-            ' ', '')
+        show_finish_list = '\n'.join(finish_names)
         delete = input(f'{show_finish_list}\n\n这 {len(finish_names)} 部 已完结是否要删除(y or n)')
         if delete == 'y':
             with open(file, 'r', encoding='utf-8') as f, open('./anime1.txt', 'w', encoding='utf-8') as f1:
@@ -40,19 +38,23 @@ def finish_anime_name_link():
                     if line.split('?')[0] not in finish_names:  # 把每一行和要删的列表进行匹配，如果没匹配到就写入,匹配到就输出
                         f1.write(line)
                     else:
-                        show_link = line.replace('?', ' ').replace('.html', '').replace('\n', '').replace('detail',
-                                                                                                          'play') + \
-                                    '/sid/1/nid/1.html'
+                        show_link = line.replace('?', ' ').replace('.html', ''). \
+                                        replace('\n', '').replace('detail', 'play') + '/sid/1/nid/1.html'
                         print(show_link)  # 直接把输出的链接变成第一集的链接
-                        show_link = line.split('?')[1].replace('.html', '').replace('\n', '').replace('detail',
-                                                                                                      'play') + \
-                                    '/sid/1/nid/1.html'
+                        show_link = line.split('?')[1].replace('.html', ''). \
+                                        replace('\n', '').replace('detail', 'play') + '/sid/1/nid/1.html'
                         # print(show_link)
                         time.sleep(0.6)
                         pyperclip.copy(show_link)
-            os.startfile(downloader_path)
-            os.remove(file)
-            os.rename('./anime1.txt', file)
+                os.startfile(downloader_path)
+                os.remove(file)
+                os.rename('./anime1.txt', file)
+
+
+def show_anime_list():
+    global show_anime_lists
+    for i in show_anime_lists:
+        print(i)
 
 
 def show_renew():
@@ -65,10 +67,11 @@ def show_renew():
 
 
 def print_all_And_add_finish(url, filename, name_d):
-    global choose, cout
+    global choose, cout, show_anime_lists
+    show_anime_lists = []
     today_date = get_today_date()
     try:
-        resp = requests.get(url, timeout=5).text
+        resp = requests.get(url, timeout=2).text
         count = obj_find_count_date.search(resp).group('count')  # 集数
         date = obj_find_count_date.search(resp).group('date')  # 更新日期
         name = obj_find_name.search(resp).group('name').strip()  # 番名
@@ -78,7 +81,7 @@ def print_all_And_add_finish(url, filename, name_d):
             renew.append(f'{count}  {name}')
         # if len(name) < name_d//4:
         #     name = name + ' '*len(name)
-        print(f'{url}  {count}/{date}     {name.center(name_d)}\n', end='')  # 地址 集数/更新日期  名字
+        show_anime_lists.append(f'{url}  {count}/{date}     {name.center(name_d)}')  # 地址 集数/更新日期  名字
         cout += 1
         # print(cout)
     except requests.exceptions.RequestException:
@@ -91,7 +94,7 @@ def main():
     names, links = get_link[0], get_link[1]
     name_d = max(map(len, names))
     # link_d = max(map(len, links))
-
+    count_retry = 1
     while 1:
         try:
             cout, renew, finish_names = 0, [], []
@@ -100,16 +103,22 @@ def main():
             with ThreadPoolExecutor(50) as f:
                 for name, link in zip(names, links):
                     f.submit(print_all_And_add_finish, link, name, name_d)
+            if count_retry >= 5:
+                print('当前网络情况不太好，请稍后再试')
+                return
             if cout != len(names):  # 判断访问的是否和追番列表数量一样
-                print("-----------运行超时-------------\n" * 4)
+                print(f"-----------第{count_retry}次运行超时-------------\n")
+                count_retry += 1
                 raise
             break
         except RuntimeError:
             pass
-    print(f'共 {len(names)} 部\n')
+    show_anime_list()
+    print(f'\n共 {len(names)} 部\n')
     show_renew()  # 展示renew列表
     finish = time.time()
     print('查询耗时' + str(round((finish - start), 1)) + 's\n')
+    finish_anime_name_link()
 
 
 if __name__ == '__main__':
@@ -118,11 +127,10 @@ if __name__ == '__main__':
         r'<li class="data"><span>状态：</span><span class="data_style">(?P<count>.*?)</span>&nbsp;/&nbsp;<em>(?P<date>.*?)</em></li>')
     obj_find_name = re.compile(r'<h2 class="title">(?P<name>.*?)</h2>', re.S)
     downloader_path = config()
+    show_anime_lists = []
     finish_names = []
     renew = []
     cout = 0
 
     main()
-    finish_anime_name_link()
     i = input('')
-
