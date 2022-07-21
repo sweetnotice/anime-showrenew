bcf5ca097023f804db49c168a9beb26ca8878938
sweetnotice <3306601284@qq.com>
2022-07-21 18:00:45 +0800

    v2.6.5
    --修复 个别番剧title识别出错的问题（这主要赖网站，它们给番命名方式真的太。。。）

--- 源码/异世界动漫网下载器.py
+++ 源码/异世界动漫网下载器.py
@@ -5,7 +5,8 @@ import requests
 import winsound
 import m3u8_downloader
 
-obj_title = re.compile(r'<title>(?P<title>.*?)(SP|第.*?集|PV|OVA|全集|高清).*?</title>', re.S)  #
+# obj_title = re.compile(r'<title>(?P<title>.*?)(SP|第.*?集|PV|OVA|全集|高清).*?</title>', re.S)  #这是从每集的名字里拆出title
+obj_title = re.compile(r'<meta name="keywords" content="(?P<title>.*?),新番,,', re.S)
 obj_name = re.compile(r'<title>(?P<name>.*?)</title>', re.S)
 obj_link = re.compile(r'","link_pre":.*?,"url":"(?P<link>.*?)","url_next":"', re.S)
 
